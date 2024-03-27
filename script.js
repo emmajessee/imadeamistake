33ce95db0c69e3445fb5ace2e52219f67d6ab014
Emma Jessee <emma.jessee@stu.glasgow.kyschools.us>
2024-03-27 10:58:34 -0500

    i messed it up the first time :(

---
+++ script.js
@@ -0,0 +1,65 @@
+//1. 
+function fozzieBear(){
+    console.log("Wocka Worka!")
+}
+fozzieBear()
+
+//2. 
+function beaker(speak){
+console.log(speak)
+console.log(speak)
+console.log(speak)
+console.log(speak)
+}
+
+beaker("Meep")
+
+//3. 
+function muppetShow(a, b){
+if (a = "Muppet", b = "Show") {
+console.log("It's time to play the music. It's time to light the lights.");
+}
+}
+muppetShow("Muppet", "Show")
+
+//4. 
+function kermit(){
+    return "Kermit The Frog"
+}
+kermit()
+
+//5. 
+function muppetShowSeasons(seasons){
+  if (seasons === 5) {
+    return true
+  } else {
+    return false
+  } }
+
+  //6. 
+  function manOrMuppet(){
+    console.log("am I a man or am I a Muppet? ");
+  }
+  manOrMuppet()
+
+  //7. 
+  rainbowConnection = () => "Someday we'll find it. The Rainbow Connection. The lovers, the dreamers and me";
+
+  rainbowConnection();
+
+  //8. no
+
+  //9. yea
+
+  //10a. 
+const newMuppetMovies = ["The Muppets", "Muppets Most Wanted"]
+
+
+
+//10b.
+const upperMovies = newMuppetMovies.map(function(movie) {
+    return movie.toUpperCase()
+  })
+  
+
+upperMovies()
